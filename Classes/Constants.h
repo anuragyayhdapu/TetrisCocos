3a7b8010584150d96f6a82b369e6dee747fc1014
Anurag <anuragyayhdapu@gmail.com>
2017-12-19 21:25:00 +0530

    Separated constants in new file

---
+++ Classes/Constants.h
@@ -0,0 +1,7 @@
+#pragma once
+
+// constants accessed from multiple files
+namespace Constant {
+	static const int NUM_OF_UNIT_BLOCKS_IN_HEIGHT = 30;
+	static const int NUM_OF_UNIT_BLOCKS_IN_WIDTH = 25;
+}
