5d50d14f5b6a4abf00182df7f0f1173e06e88497
Anurag <anuragyayhdapu@gmail.com>
2017-12-20 23:00:08 +0530

    rotation & spawn constants added

--- Classes/Constants.h
+++ Classes/Constants.h
@@ -1,7 +1,24 @@
 #pragma once
 
+#include "BoardPos.h"
+
 // constants accessed from multiple files
-namespace Constant {
-	static const int NUM_OF_UNIT_BLOCKS_IN_HEIGHT = 30;
-	static const int NUM_OF_UNIT_BLOCKS_IN_WIDTH = 25;
+namespace Constant
+{
+	const int NUM_OF_UNIT_BLOCKS_IN_HEIGHT = 30;
+	const int NUM_OF_UNIT_BLOCKS_IN_WIDTH = 25;
+
+	enum RotationState
+	{
+		TOP, RIGHT, BOTTOM, LEFT
+	};
+
+	const BoardPos SPAWN_POSITION(15, 0);
+	const RotationState SPAWN_ROTATION_STATE = TOP;
+
+	const std::map<RotationState, RotationState> rightRotate = {
+		{TOP, RIGHT}, {RIGHT, BOTTOM}, {BOTTOM, LEFT}, {LEFT, TOP} };
+
+	const std::map<RotationState, RotationState> leftRotate = {
+		{ TOP, LEFT },{ RIGHT, TOP },{ BOTTOM, RIGHT },{ LEFT, TOP } };
 }
