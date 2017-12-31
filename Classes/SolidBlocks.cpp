ee3b756df6fecd728908db007d53d3ff0c2c5564
Anurag <anuragyayhdapu@gmail.com>
2017-12-31 23:30:31 +0530

    implementing line clearing of solidblocks using 'cascading style'

---
+++ Classes/SolidBlocks.cpp
@@ -0,0 +1,144 @@
+#include "SolidBlocks.h"
+
+
+bool SolidBlocks::init()
+{
+	if (!Node::init()) {
+		return false;
+	}
+
+	// initialize bucket
+	for (size_t i = 0; i < Constant::BUCKET_HEIGHT; i++)
+		for (size_t j = 0; j < Constant::BUCKET_WIDTH; j++)
+			bucket[i][j] = nullptr;
+
+	return true;
+}
+
+
+bool SolidBlocks::rowFilled(int rowNum) {
+
+	for (size_t i = 0; i < Constant::BUCKET_WIDTH; i++)
+		if (bucket[rowNum][i] == nullptr)
+			return false;
+
+	return true;
+}
+
+
+void SolidBlocks::add(const std::vector<UnitBlock*>& newBlocks) {
+	// add to internal solidTetrominos also
+	SolidTetromino st;
+
+	for each (auto newBlock in newBlocks)
+	{
+		auto solidBlock = UnitBlock::create(newBlock->getX(), newBlock->getY(), newBlock->getColor());
+		bucket[solidBlock->getX()][solidBlock->getY()] = solidBlock;
+		this->addChild(solidBlock);
+
+		st.unitBlocks.push_back(solidBlock);
+	}
+
+	solidTetrominos.push_back(st);
+}
+
+
+void SolidBlocks::drawSolidBlocks(std::vector<int> rows)
+{
+	// draw entire matrix
+	if (rows.size() == 0)
+	{
+		for (size_t i = 0; i < Constant::BUCKET_HEIGHT; i++)
+		{
+			for (size_t j = 0; j < Constant::BUCKET_WIDTH; j++)
+			{
+				if (bucket[i][j] != nullptr)
+					bucket[i][j]->drawBlock();
+			}
+		}
+	}
+
+	// draw selected rows
+	else
+	{
+		for each (auto i in rows)
+		{
+			for (size_t j = 0; j < Constant::BUCKET_WIDTH; j++)
+			{
+				if (bucket[i][j] != nullptr)
+					bucket[i][j]->drawBlock();
+			}
+		}
+	}
+}
+
+
+void SolidBlocks::updateSolidBlocks(Tetromino * movableTetromino)
+{
+	// adds all blocks of this tetromino in solidblocks matrix
+	add(movableTetromino->getUnitBlocksVec());
+
+	// check if any rows are filled
+	std::set<int> filledRows;
+	for each (auto block in movableTetromino->getUnitBlocksVec())
+	{
+		auto y = block->getY();
+		if (rowFilled(y) == true)
+			filledRows.insert(y);
+	}
+
+	// delete thows rows
+	for each (auto i in filledRows)
+	{
+		for (size_t j = 0; j < Constant::BUCKET_WIDTH; j++)
+		{
+			// clear drawNodes
+			bucket[i][j]->clearDrawnBlock();
+
+
+			// find solidTetromino for this unitBlock
+			for (size_t k = 0; k < solidTetrominos.size(); k++)
+			{
+				std::vector<UnitBlock*>& blocks = solidTetrominos.at(k).unitBlocks;
+				for (size_t t = 0; t < blocks.size(); t++)
+				{
+					if (blocks.at(t) == bucket[i][j])
+					{
+						blocks.at(t) = nullptr;	// decrease referenc count
+						blocks.erase(blocks.begin() + t);
+					}
+				}
+
+				if (blocks.size() == 0)
+				{
+					// TODO: erase this tetromino
+				}
+			}
+
+			// set nullptr in matrix
+			bucket[i][j] = nullptr;
+		}
+	}
+
+	// shift above rows down
+
+	// get lowest x from filled rows
+	int lowX = Constant::BUCKET_HEIGHT;
+	for each (auto x in filledRows)
+	{
+		if (x < lowX)
+			lowX = x;
+	}
+
+	// move all tetromino of the lowest row, repeat recursively till top
+	for (int i = lowX + 1; i >= 0; i--)
+	{
+		for (int j = 0; j < Constant::BUCKET_WIDTH; j++)
+		{
+			//	TODO: find this u* in st*
+			// TODO: move that st* and update in matrix
+			// TODO: repeat for entire row
+		}
+	}
+
+}
---
+++ Classes/SolidBlocks.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include "Constants.h"
+#include "BoardPos.h"
+#include "UnitBlock.h"
+#include "Tetromino.h"
+#include <vector>
+#include <set>
+
+class SolidBlocks : public cocos2d::Node {
+
+private:
+	// unitBlocks hudled together
+	struct SolidTetromino {
+		std::vector<UnitBlock*> unitBlocks;	
+		bool moved = false;
+	};
+	std::vector<SolidTetromino> solidTetrominos;
+
+	UnitBlock * bucket[Constant::BUCKET_HEIGHT][Constant::BUCKET_WIDTH];
+
+	// checks if a single row is filled
+	bool rowFilled(int rowNum);
+
+	// add new blocks in solidBlock
+	void add(const std::vector<UnitBlock*>& newBlocks);
+
+public:
+
+	virtual bool init();
+	CREATE_FUNC(SolidBlocks);
+
+	bool find(BoardPos bPos) {
+		return bucket[bPos.x][bPos.y] != nullptr;
+	}
+
+	// checks which rows are filled, deletes them, and shifts top rows down to cover up the space
+	void updateSolidBlocks(Tetromino* movableTetromino);
+
+	// draws / redraws solidBlocks
+	void drawSolidBlocks(std::vector<int> rows = {});
+};
