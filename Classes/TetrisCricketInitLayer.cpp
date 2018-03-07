f81fd511ed87704f6089f2d1bf40861b5daf639b
Anurag <anuragyayhdapu@gmail.com>
2018-03-07 23:46:56 +0530

    completed TetrisCricketInitLayer

--- Classes/TetrisCricketInitLayer.cpp
+++ Classes/TetrisCricketInitLayer.cpp
@@ -22,14 +22,29 @@ TetrisCricketInitLayer * TetrisCricketInitLayer::create(std::function<void(cocos
 bool TetrisCricketInitLayer::init()
 {
 	auto size = Director::getInstance()->getVisibleSize();
-	if (!LayerColor::initWithColor(Color4B(0, 0, 0, 128), size.width, size.height))
+	if (!LayerColor::initWithColor(Color4B(0, 0, 0, 240), size.width, size.height))
 	{
 		return false;
 	}
 
 	auto midpos = cocos2d::Vec2(size.width / 2, size.height / 2);
-	addChild(TetrisButton::create(startFunc, "start", Color4F::BLUE, Vec2(midpos.x, midpos.y), 1.0f, FontAlign::MIDDLE, FontColorPattern::RANDOM_BLOCK, FontDrawPattern::SOLID));
+	addChild(TetrisButton::create(startFunc, "play", Color4F::BLUE, Vec2(midpos.x, size.height * 0.3), 1.5f, FontAlign::MIDDLE, FontColorPattern::RANDOM_BLOCK, FontDrawPattern::SOLID));
+
+	TetrisFont *p1, *p2, *t1, *t2, *h;
+	DrawNode *txtDrawNode;
+
+	addChild(txtDrawNode = DrawNode::create());
+	addChild(h = TetrisFont::create("first inning", Color4F::ORANGE, Vec2(midpos.x, size.height * 0.8), 1.5f, FontColorPattern::RANDOM_BLOCK, FontDrawPattern::SOLID, FontAlign::MIDDLE));
+	addChild(p1 = TetrisFont::create("player1", Color4F::GRAY, Vec2(midpos.x, size.height * 0.6), 0.8f, FontColorPattern::FULL, FontDrawPattern::SOLID, FontAlign::RIGHT));
+	addChild(p2 = TetrisFont::create("player2", Color4F::GRAY, Vec2(midpos.x, size.height * 0.5), 0.8f, FontColorPattern::FULL, FontDrawPattern::SOLID, FontAlign::RIGHT));
+	addChild(t1 = TetrisFont::create("bowling", Color4F::ORANGE, Vec2(size.width * 0.53, size.height * 0.6), 1.0f, FontColorPattern::RANDOM_BLOCK, FontDrawPattern::SOLID, FontAlign::LEFT));
+	addChild(t2 = TetrisFont::create("batting", Color4F::ORANGE, Vec2(size.width * 0.53, size.height * 0.5), 1.0f, FontColorPattern::RANDOM_BLOCK, FontDrawPattern::SOLID, FontAlign::LEFT));
 
 
+	h->write(txtDrawNode);
+	p1->write(txtDrawNode);
+	p2->write(txtDrawNode);
+	t1->write(txtDrawNode);
+	t2->write(txtDrawNode);
 	return true;
 }
